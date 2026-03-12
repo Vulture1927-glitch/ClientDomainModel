c7c6c4e3fa0acba60d2fef3add2e289601c5312b
Isaac Stumm <isaac.stumm@students.snow.edu>
2026-03-12 14:45:14 -0600

    Finished up to Method

---
+++ ClientDomainModel/CompanyManagment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientDomainModel
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+
+        public Client(string name = "Unkown", string email = "None entered")
+        {
+            Name = name;
+            Email = email;
+            Id = Guid.NewGuid();
+            CreatedOn = DateTime.Now;
+            IsActive = true;
+        }
+
+        public Client(Guid id, string name, string email, DateTime createdOn, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            CreatedOn = createdOn;
+            IsActive = isActive;
+        }
+
+        public void GetSummary(Client client)
+        {
+            Console.WriteLine(client.Name);
+            Console.WriteLine(client.Email);
+            Console.WriteLine(client.Id);
+            Console.WriteLine(client.CreatedOn);
+            Console.WriteLine(client.IsActive);
+        }
+    }
+}
--- ClientDomainModel/Program.cs
+++ ClientDomainModel/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using ClientDomainModel;
+public class Program
+{
+    static void Main()
+    {
+        Client client = new Client("Isaac", "someEmail");
+        client.GetSummary(client);
+
+    }
+}
